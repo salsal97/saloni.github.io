af7e29c7a13c92ced64432c194aef2082663bf2a
Saloni Goel <saloni97@vt.edu>
2019-05-08 12:29:13 -0400

    added us-state maps

---
+++ us-states/map_script.js
@@ -0,0 +1,76 @@
+// Special Thanks to @thesourabh
+// Look up this here - https://github.com/thesourabh/thesourabh.github.io/blob/master/travel/maps/common/map_script.js
+
+// Storing them so that they don't have to be fetched repeatedly
+let stateDoms = $("path, circle"),
+  infoBoxDom = $('#info-box');
+
+// If any of the state IDs match those in our file, apply the relevant CSS class
+$.each(stateDoms, function (key, value) {
+  if (value.id in states) {
+    let state = states[value.id];
+    if ('visited' in state) {
+      value.classList.add("visited");
+    } else if ('lived' in state) {
+      value.classList.add("lived");
+    }
+  }
+});
+
+/*
+let dateMap = {};
+$.each(states, function (stateId, state) {
+  if ('passed' in state) {
+    if (state['passed'] in dateMap) {
+      dateMap[state['passed']].push(stateId + '-passed');
+    } else {
+      dateMap[state['passed']] = [stateId + '-passed'];
+    }
+  } else if ('visited' in state) {
+    if (state['visited'] in dateMap) {
+      dateMap[state['visited']].push(stateId + '-visited');
+    } else {
+      dateMap[state['visited']] = [stateId + '-visited'];
+    }
+  }
+});
+let sortedDates = Object.keys(dateMap).sort();
+console.log(sortedDates);
+$.each(sortedDates, function (index, date) {
+  var eventsOnDate = dateMap[date];
+  setTimeout(function () {
+    $.each(eventsOnDate, function (indexx, stateEvent) {
+      var stateAndEvent = stateEvent.split("-");
+      var stateDOM = $('#' + stateAndEvent[0])[0];
+      stateDOM.classList = [stateAndEvent[1]];
+      console.log(stateAndEvent[0] + stateDOM.classList);
+    });
+  }, 500 * index);
+});
+*/
+
+// On hover, show in the info box with info for the state
+stateDoms.hover(function (e) {
+  let stateId = e.target.id,
+    state = states[stateId] || {},
+    infoBoxData = "",
+    dateToDisplay = state.visited || state.lived;
+  infoBoxData += (state.name || stateId);
+  if (dateToDisplay) {
+    infoBoxData += "<br>" + dateToDisplay;
+  }
+  if (state.description) {
+    infoBoxData += "<br>" + state.description;
+  }
+  infoBoxDom.css('display', 'block');
+  infoBoxDom.html(infoBoxData);
+});
+
+stateDoms.mouseleave(function (e) {
+  infoBoxDom.css('display', 'none');
+});
+
+$(document).mousemove(function (e) {
+  infoBoxDom.css('top', e.pageY - infoBoxDom.height() - 30);
+  infoBoxDom.css('left', e.pageX - (infoBoxDom.width()) / 2);
+}).mouseover();
---
+++ us-states/state_data.js
@@ -0,0 +1,57 @@
+var states = {
+  VA: {
+    name: "Virginia",
+    lived: "2017-08-17 to 2019-05-14",
+    description: "Virginia Tech Class of 2019"
+  },
+  FL: {
+    name: "Florida",
+    visited: "2013-05-16",
+    description: "Disney, Universal Studios, Sea World and Miami"
+  },
+  DC2: {
+    name: "Washington, District of Columbia",
+    visited: "2013-05-10",
+    description: "Travel"
+  },
+  MD: {
+    name: "Maryland",
+    visited: "2017-05-01",
+    description: "Visit University of Maryland"
+  },
+  NY: {
+    name: "New York",
+    visited: "2013-05-12",
+    description: "Empire State of Mind"
+  },
+  TX: {
+    name: "Texas",
+    visited: "2013-04-20",
+    description: "BMC Software Sidekick for mother :P"
+  },
+  CA: {
+    name: "California",
+    lived: "2017-08-20 to 2017-12-28",
+    description: "Tensyr Inc Co-op"
+  },
+  OR: {
+    name: "Oregon",
+    visited: "2018-11-22",
+    description: "Thanksgiving 2018 with the Blancetts"
+  },
+  AZ: {
+    name: "Arizona",
+    visited: "2017-05-10",
+    description: "ASU Visit 2017"
+  },
+  SC: {
+    name: "South Carolina",
+    visited: "2017-03-04",
+    description: "Hilton Head Island Spring Break"
+  },
+  WA: {
+    name: "Seattle, WA",
+    visited: "2018-10-18",
+    description: "Microsoft Visit"
+  }
+}
